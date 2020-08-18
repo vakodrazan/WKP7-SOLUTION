704459726eb74e4437ff7f0e114815d4ec86ae15
Pinois <>
2020-08-18 13:52:46 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# WKP7 - Library
+
+### Hey team!
+
+Here is the repo to clone for coding along while we correct the WKP7. Cheers
---
+++ script.js
@@ -0,0 +1,25 @@
+let books = [];
+
+// add an element to the list with the form
+// The element is added on the list
+// reset the form after submission
+// Not add an empty element
+// delete an element
+// edit the state of an element
+// save new element to local storage
+// save the new state of object in local storage
+// form validation?
+
+const showBooks = () => {};
+
+const addBook = e => {};
+
+const handleClick = e => {};
+
+const deleteBook = id => {};
+
+const updateRead = id => {};
+
+const initLocalStorage = () => {};
+
+const updateLocalStorage = () => {};
